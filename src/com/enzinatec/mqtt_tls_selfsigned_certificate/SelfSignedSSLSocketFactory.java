ee9fc200090c7cc457dfab6255021aac36b66992
Manuel Domínguez-Dorado <manolodd@gmail.com>
2017-02-14 20:20:36 +0100

    Update SelfSignedSSLSocketFactory.java

--- src/com/enzinatec/mqtt_tls_selfsigned_certificate/SelfSignedSSLSocketFactory.java
+++ src/com/enzinatec/mqtt_tls_selfsigned_certificate/SelfSignedSSLSocketFactory.java
@@ -1,16 +1,21 @@
 package com.enzinatec.mqtt_tls_selfsigned_certificate;
 
-/*
- * ****************************************************************
- *  Copyright (C) 2015 enZina Technologies.
+
+/* 
+ * Copyright 2015 (C) enZina Technologies
+ * manuel.dominguez@enzinatec.com
  *
- *  This software is property of enZina Technologies. You are 
- *  granted only the right of using it, but not to modify it 
- *  without the explicit writen permission of enZina Technologies.
- *  For more information contact to enZina technologies at:
- *  - http://www.enzinatec.com 
- *  - info@enzinatec.com
- * ****************************************************************
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 
